1edf2f5fd7752d41d47b217526c9a1fa60235ecd
yuma <yuma.matsumori@x-factory.jp>
2023-07-09 01:08:29 +0900

    いろいろ

--- script.js
+++ script.js
@@ -11,31 +11,43 @@ let TextNum = (MaxHeight / Textheight).toFixed() * 3;
 const TargetElement = document.getElementById("app");
 let AppWidth = 0;
 
-const CreateText = () => {
-    for (var i = 0; i < TextNum; i++) {
-        const TextP = document.createElement("p");
-        TextP.classList.add("text");
-        TextP.setAttribute("id", "Scale" + i);
-        TextP.textContent = "Transform";
-        TargetElement.appendChild(TextP);
-        TextWidth = TextP.clientWidth;
-    }
-    AppWidth = TextWidth * 3;
-    TargetElement.style.width = AppWidth + "px";
+const GenerationText = (num) => {
+    console.log("num: " + num);
+    const base = 6;
+    for (var j = 0; j < num; j++) {
+        const group = document.createElement("div");
+        group.classList.add("group");
+        group.setAttribute("id", "G" + j);
+        TargetElement.appendChild(group);
+        for (var i = 0; i < base; i++) {
+            const TextP = document.createElement("p");
+            TextP.classList.add("text");
+            TextP.textContent = "Bimyoo/YumaMatsumori/";
+            group.appendChild(TextP);
+            TextWidth = TextP.clientWidth;
+        };
+    };
 };
 
-const AppStretch = () => {
-    TargetElement.style.transform = "scale(" + MaxWidth / AppWidth + ",1)";
+const windowSizeCheck = () => {
+    let width = document.documentElement.clientWidth,
+        height = document.documentElement.clientHeight;
+    return { width, height };
 };
 
-CreateText();
-AppStretch();
+const HowManyToCreate = () => {
+    return (
+        (windowSizeCheck().height / Textheight).toFixed(0)
+    )
+};
+
+const randomSpeed = () => {
+    const max = 6,
+        min = 4;
+    return (Math.floor(Math.random() * (max - min) + min)) + "s";
+}
 
 window.addEventListener("resize", (ResizeHeight) => {
-    MaxHeight = document.documentElement.clientHeight;
-    MaxWidth = document.documentElement.clientWidth;
-    AppStretch();
-    console.log("MaxWidth: " + MaxWidth);
 });
 
 window.addEventListener("mousemove", (mousemove) => {
@@ -45,43 +57,7 @@ window.addEventListener("mousemove", (mousemove) => {
     HeightScale = ((mouseY / MaxHeight) * 2).toFixed(2);
     let WidthScaleR = (((mouseX - MaxWidth) / -MaxWidth) * 2).toFixed(2);
     let HeightScaleR = (((mouseY - MaxHeight) / -MaxHeight) * 2).toFixed(2);
-    for (var i = 0; i < TextNum; i = i + 3) {
-        document.getElementById("Scale" + i).style.transformOrigin = "center left";
-        document.getElementById("Scale" + i).style.fontWeight = (mouseX / MaxWidth) * 900;
-
-        if (i % 6 == 0) {
-            document.getElementById("Scale" + i).style.transform =
-                "scale(" + WidthScale + "," + HeightScale + ")";
-            document.getElementById("Scale" + i).style.fontVariationSettings = "wght" + 900;
-        }
-        else {
-            document.getElementById("Scale" + i).style.transform =
-                "scale(" + WidthScale + "," + HeightScaleR + ")";
-        }
-    }
-    for (var i = 1; i < TextNum; i = i + 3) {
-        document.getElementById("Scale" + i).style.transformOrigin =
-            "center center";
-            document.getElementById("Scale" + i).style.fontWeight = (mouseX / MaxWidth) * 900;
-        if (i % 6 == 1) {
-            document.getElementById("Scale" + i).style.transform =
-                "scale(" + WidthScaleR + "," + HeightScale + ")";
-        }
-        else {
-            document.getElementById("Scale" + i).style.transform =
-                "scale(" + WidthScaleR + "," + HeightScaleR + ")";
-        }
-    }
-    for (var i = 2; i < TextNum; i = i + 3) {
-        document.getElementById("Scale" + i).style.transformOrigin = "center right";
-        document.getElementById("Scale" + i).style.fontWeight = (mouseX / MaxWidth) * 900;
-        if (i % 6 == 2) {
-            document.getElementById("Scale" + i).style.transform =
-                "scale(" + WidthScale + "," + HeightScale + ")";
-        }
-        else {
-            document.getElementById("Scale" + i).style.transform =
-                "scale(" + WidthScale + "," + HeightScaleR + ")";
-        }
-    }
+    document.getElementById("app").style.fontWeight = (mouseX / windowSizeCheck().width).toFixed(2) * 900;
 });
+
+GenerationText(HowManyToCreate());

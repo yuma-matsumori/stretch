4e334af6f4122abf5b9606d14826cd00bb0260a2
yuma-matsumori <yuma.matsumori@x-factory.jp>
2023-07-10 16:56:28 +0900

    いらんコードをけしたりした

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 "use strict";
-let MaxHeight = document.documentElement.clientHeight;
-let MaxWidth = document.documentElement.clientWidth;
 let mouseX = 0;
 let mouseY = 0;
-let WidthScale = 0;
-let HeightScale = 0;
 let TextWidth = 0;
 const Textheight = 64;
-let TextNum = (MaxHeight / Textheight).toFixed() * 3;
 const TargetElement = document.getElementById("app");
-let AppWidth = 0;
+
+
 
 const GenerationText = (num) => {
     console.log("num: " + num);
-    const base = 6;
+    const base = 8;
     for (var j = 0; j < num; j++) {
         const group = document.createElement("div");
         group.classList.add("group");
@@ -53,11 +49,7 @@ window.addEventListener("resize", (ResizeHeight) => {
 window.addEventListener("mousemove", (mousemove) => {
     mouseX = event.pageX;
     mouseY = event.pageY;
-    WidthScale = (mouseX / MaxWidth + 0.5).toFixed(2);
-    HeightScale = ((mouseY / MaxHeight) * 2).toFixed(2);
-    let WidthScaleR = (((mouseX - MaxWidth) / -MaxWidth) * 2).toFixed(2);
-    let HeightScaleR = (((mouseY - MaxHeight) / -MaxHeight) * 2).toFixed(2);
-    document.getElementById("app").style.fontWeight = (mouseX / windowSizeCheck().width).toFixed(2) * 900;
+    TargetElement.style.fontWeight = (mouseX / windowSizeCheck().width).toFixed(2) * 900;
 });
 
 GenerationText(HowManyToCreate());
